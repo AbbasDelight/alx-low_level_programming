67a1570140d431ea9ffaea3acc352fb8a3501bcc
AbbasDelight <arotore@gmail.com>
2023-03-02 08:43:31 -0800

    More pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,6 +12,8 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
 		if (str[index - 1] == ' ' ||
 		if (str[index - 1] == '\t' ||
 		if (str[index - 1] == '\n' ||
@@ -27,9 +29,9 @@ char *cap_string(char *str)
 		if (str[index - 1] == '}' ||
 		index == 0)
 		str[index] -= 32;
+
 		index++;
 	}
 
 	return (str);
 }
-
